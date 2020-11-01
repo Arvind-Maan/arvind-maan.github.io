f20a8addc6e2d505d45d97de099c0e7d534352f1
Arvind Maan <arvindmaan1199@gmail.com>
2020-10-31 23:50:30 -0500

    Create initial react project

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledApp = styled.div`
+	background: e5e5e5;
+	height: 100%;
+	width: 100%;
+`;
+
+const App = () => {
+	return <StyledApp className="App"></StyledApp>;
+};
+
+export default App;
